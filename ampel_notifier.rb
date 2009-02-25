185777509952300ad75912ebe58aebbc0265ee11
Dirk Breuer <dirk.breuer@gmail.com>
2009-02-25 09:06:26 +0100

    bugfixing

--- ampel_notifier.rb
+++ ampel_notifier.rb
@@ -6,29 +6,25 @@ class AmpelNotifier
     @port = 80
   end
   
-  def server(options = {})
+  def remote_server(options = {})
     @host = options[:host]
     @port = options[:port]
   end
   
   def build_started(build)
-    Net::HTTP.get_print @host, '/ampel_server/index?actions[yellow]=On', @port
+    Net::HTTP.get @host, '/ampel_server/index?actions[yellow]=On', @port
   end
   
   def build_finished(build)
-    Net::HTTP.get_print @host, '/ampel_server/index?actions[yellow]=Off&actions[green]=On', @port
+    Net::HTTP.get @host, '/ampel_server/index?actions[yellow]=Off', @port
   end
   
-  def build_fixed(build)
-    Net::HTTP.get_print @host, '/ampel_server/index?actions[yellow]=Off&actions[green]=On', @port
+  def build_fixed(build, previous_build)
+    Net::HTTP.get @host, '/ampel_server/index?actions[red]=Off&actions[green]=On', @port
   end
   
-  def build_broken(error)
-    Net::HTTP.get_print @host, '/ampel_server/index?actions[yellow]=Off&actions[green]=Off&actions[red]=On&actions[signal]=23', @port
-  end
-  
-  def build_loop_failed(error)
-    Net::HTTP.get_print @host, '/ampel_server/index?actions[yellow]=Off&actions[green]=Off&actions[red]=On', @port
+  def build_broken(build, previous_build)
+    Net::HTTP.get @host, '/ampel_server/index?actions[green]=Off&actions[red]=On&actions[signal]=23', @port
   end
   
 end
