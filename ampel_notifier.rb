f0663e19f9221a1802e1f0d9d750e3a12b1b7513
Dirk Breuer <>
2009-07-28 16:33:52 +0200

    Plugin has to be explicitly activated in each project

--- ampel_notifier.rb
+++ ampel_notifier.rb
@@ -2,8 +2,7 @@ require 'net/http'
 
 class AmpelNotifier
   def initialize(project)
-    @host = "ampelserver.yourproject.de"
-    @port = 80
+    @activated = false
   end
   
   def remote_server(options = {})
@@ -11,21 +10,39 @@ class AmpelNotifier
     @port = options[:port]
   end
   
-  def build_started(build)
-    Net::HTTP.get @host, '/ampel_server/index?actions[yellow]=On', @port
+  def activate!
+    @activated = true
   end
   
-  def build_finished(build)
-    Net::HTTP.get @host, '/ampel_server/index?actions[yellow]=Off', @port
+  def activated?
+    @activated
+  end
+
+  def method_missing(meth, *args, &blk)
+    if meth.to_s =~ /build_\w+/
+      activated? and send("_#{meth}", *args)
+    else
+      super
+    end
   end
   
-  def build_fixed(build, previous_build)
-    Net::HTTP.get @host, '/ampel_server/index?actions[red]=Off&actions[green]=On', @port
-  end
+  private
   
-  def build_broken(build, previous_build)
-    Net::HTTP.get @host, '/ampel_server/index?actions[green]=Off&actions[red]=On&actions[signal]=23', @port
-  end
+    def _build_started(build)
+      Net::HTTP.get @host, '/ampel_server/index?actions[yellow]=On', @port
+    end
+  
+    def _build_finished(build)
+      Net::HTTP.get @host, '/ampel_server/index?actions[yellow]=Off', @port
+    end
+  
+    def _build_fixed(build, previous_build)
+      Net::HTTP.get @host, '/ampel_server/index?actions[red]=Off&actions[green]=On', @port
+    end
+  
+    def _build_broken(build, previous_build)
+      Net::HTTP.get @host, '/ampel_server/index?actions[green]=Off&actions[red]=On&actions[signal]=23', @port
+    end
   
 end
 
